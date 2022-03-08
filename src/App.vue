3b6bdc0ad4a8b3affce15d9fa65eba262768f371
Zhao <zhaoshuna@mozat.com>
2022-03-08 15:01:06 +0800

    完成了手写Vuex

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    123
+    <p>{{this.$store.state.num}}</p>
+    <p>getter:{{this.$store.getters.getNum}}</p>
+    <button @click="add">+1</button>
+    <button @click="asyncAdd">异步+2</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    add() {
+      this.$store.commit('incre', 1)
+    },
+    asyncAdd() {
+      this.$store.dispatch('asycIncre', 2)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
